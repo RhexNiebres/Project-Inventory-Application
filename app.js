5f538a07aa4c5e621546369f7727e89f28fc8acc
RhexNiebres <rhexniebres@gmail.com>
2025-03-03 13:29:36 -0800

    add dotenv configuration

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require ('express');
 const app = express();
 const pokemonRouter = require("./routes/pokemonRouter");
+const path = require("node:path");
+require("dotenv").config();
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use("/", pokemonRouter);
