1f48cea10128ea629e94747f925939e07864aebd
RhexNiebres <rhexniebres@gmail.com>
2025-03-03 17:00:31 -0800

    chore: add router for adding a pokemon and a trainer

--- routes/pokemonRouter.js
+++ routes/pokemonRouter.js
@@ -4,6 +4,8 @@ const pokemonTrainerController = require('../controllers/pokemonTrainerControlle
 
 router.get('/', pokemonTrainerController.getIndex)
 router.get('/pokemon', pokemonTrainerController.getPokemons);
-router.get('/trainers', pokemonTrainerController.getTrainers)
+router.get('/trainers', pokemonTrainerController.getTrainers);
+router.post('/pokemon/add', pokemonTrainerController.postAddPokemon);
+router.post('/trainer/add', pokemonTrainerController.postAddTrainer);
 
 module.exports = router;
